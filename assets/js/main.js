f901635caa615aed84ce2c2219aba73333f0da69
FiXxer2323 <kabarcz.kristof23@gmail.com>
2023-06-26 18:21:41 +0200

    Nav Hide Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
 const aside = document.querySelector('aside');
-const button = document.querySelector('main button');
+const Button = document.querySelector('main button');
+
+
 //console.log(aside);
 
 function showNav() {
-    aside.innerHTML=`<a href="/">Tartalom Lista</a>
-<a href="/">Új Tartalom</a>
-<a href="/">Hír Lista</a>
-<a href="/">Új Hír</a>
-<a href="/">Galéria Lista</a>
-<a href="/">Új Galéria</a>`;
+    aside.innerHTML=`<a href="/" >Tartalom Lista</a>
+    <a href="/" >Új Tartalom</a>
+    <a href="/" >Hír Lista</a>
+    <a href="/" >Új Hír</a>
+    <a href="/" >Galéria Lista</a>
+    <a href="/" >Új Galéria</a>`;
+    const hideNav = document.getElementById("Nav");      
+    hideNav.remove();
 }
+
+function nav() {
+    aside.hidden=`<a href="/" >Tartalom Lista</a>
+    <a href="/" >Új Tartalom</a>
+    <a href="/" >Hír Lista</a>
+    <a href="/" >Új Hír</a>
+    <a href="/" >Galéria Lista</a>
+    <a href="/" >Új Galéria</a>`;
+    const hideNav = document.getElementById("Nav");      
+    hideNav.remove();
+    console.log("Eltüntettem");
+    const message = document.getElementById("Mess").innerHTML = "Eltüntettem!";
+}
+
+
